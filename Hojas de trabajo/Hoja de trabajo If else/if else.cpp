4613bb2aa8c461cec5d7e3fbace25647c318234f
moisesafarfan <farfan6moises@gmail.com>
2026-03-25 08:43:54 -0600

    Revert "Merge branch 'main' of https://github.com/moisesafarfan/PROGRAMACION-1"
    This reverts commit b1485a9aa8dc48f137b19681c9700cde13d10329, reversing
    changes made to f3ef87bdf6cc0b6a56aaa2572bd847d9a36ea158.

---
+++ Hojas de trabajo/Hoja de trabajo If else/if else.cpp
@@ -0,0 +1,18 @@
+#include <isotream>
+using namespace ntd;
+
+int main () {
+
+    char nombre
+    int edad
+    double promedio
+    char clubprogra
+
+ cout << "Cual es tu nombre: ";
+ cin >> nombre
+
+ cout << "Cual es tu edad: ";
+ cin >> edad;
+
+
+}
